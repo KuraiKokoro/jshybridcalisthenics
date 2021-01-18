1f0e73ba93fafaba1df187dfda80ba3db360ffeb
Adrian Henson <AdrianHenson6@gmail.com>
2021-01-17 19:32:42 -0500

    yes

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 /* jshint esversion: 8 */
 
 const Discord = require("discord.js");
-const config = require("./config.json");
+const {prefix, token} = require("./config.json");
+const ytsr = require('ytsr');
 
 
 const client = new Discord.Client();
@@ -10,12 +11,37 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', (msg) => {
-    if (msg.content === 'ping'){
-        msg.reply('pong');
+client.on('message', async (msg) => {
+    if (!msg.content.startsWith(prefix) || msg.author.bot ) return;
+
+    const args = msg.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if ( command === "ping" ) {
+        msg.channel.send("Pong...");
     }
+
+    if ( command === "search" ) {
+        const query = args.join(" ");
+        msg.channel.send(await ytDlSearch(query));
+    }
+
 });
 
-client.login(config.token);
+
+const ytDlSearch = async (query) => {
+    let results = [];
+    ytsr(`"Hybrid Calisthenics" ${query}`).then((result) => {
+        for ( let item of result.items) {
+            if (item.type == "video" && item.author.url == 'https://www.youtube.com/channel/UCeJFgNahi--FKs0oJyeRDEw'){
+            results.push(item);    
+        }
+        }
+    }).then(() => {
+        return results;
+    });
+};
 
 
+
+client.login(token);
